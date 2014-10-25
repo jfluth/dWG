f373377aad66c7f61b3e8c228f6aea5b38cea4af
pwlong <github@thelongs.ws>
2014-10-25 15:29:32 -0700

    i am afraid to commit

--- nexys4_bot_if.v
+++ nexys4_bot_if.v
@@ -91,8 +91,6 @@ module  nexys4_bot_if (
 	
 	// Incoming to I/O block
 	always @(posedge clk /*or negedge rst*/) begin
-<<<<<<< HEAD
-=======
 		if (Button[0]) LED[0]  <= 1'b1; else LED[0] <= 1'b0;
 		
 		if (BotInterrupt && (count < 1000000000)) begin 
@@ -101,7 +99,7 @@ module  nexys4_bot_if (
         end
         else LED[15] <= 1'b0;
         LED[5] <= BotInterrupt;
->>>>>>> 47fe3cd93d9b369b3c60b92ed4b6e96a37226a25
+
 		
 	  //Dig0 <= 8'd16;
 		/*if (!rst) begin // see comment above re: agnostic wrt: reste active high/low

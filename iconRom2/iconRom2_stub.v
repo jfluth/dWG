c835b2a7da2ce8d52bb903e0377d46fb0a81dfe7
Paul Long <github@thelongs.ws>
2014-10-31 13:52:26 -0700

    i

---
+++ iconRom2/iconRom2_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2014.3 (win64) Build 1034051 Fri Oct  3 17:14:12 MDT 2014
+// Date        : Fri Oct 31 10:18:47 2014
+// Host        : IPA running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub {c:/Users/pwl/Git Repos/540/proj2_src/iconRom2/iconRom2_stub.v}
+// Design      : iconRom2
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_2,Vivado 2014.3" *)
+module iconRom2(clka, ena, addra, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,ena,addra[8:0],douta[11:0]" */;
+  input clka;
+  input ena;
+  input [8:0]addra;
+  output [11:0]douta;
+endmodule
---
+++ iconRom2/iconRom2_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2014.3 (win64) Build 1034051 Fri Oct  3 17:14:12 MDT 2014
+-- Date        : Fri Oct 31 10:18:47 2014
+-- Host        : IPA running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Users/pwl/Git Repos/540/proj2_src/iconRom2/iconRom2_stub.vhdl}
+-- Design      : iconRom2
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity iconRom2 is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 8 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 11 downto 0 )
+  );
+
+end iconRom2;
+
+architecture stub of iconRom2 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,addra[8:0],douta[11:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_2,Vivado 2014.3";
+begin
+end;

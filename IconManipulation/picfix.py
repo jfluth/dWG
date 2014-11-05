0d6e180b14647a2661754d430275e8de010f5ff6
Paul Long <github@thelongs.ws>
2014-11-04 21:19:14 -0800

    Added cleaned up picfix.py script

--- IconManipulation/picfix.py
+++ IconManipulation/picfix.py
@@ -1,13 +1,18 @@
 #! /usr/bin/python
 '''
-File Name: pic2array.py
+File Name: picfix.py
 Author:	Paul long <pwl@pdx.edu>
-Python Version: 2.7
+
 Date: November 2014
+
+Required:
+	Python Version: 2.7: https://www.python.org/download/releases/2.7/
+	Python image library: http://www.pythonware.com/products/pil/
+	
 Description:
-	This script loads in an image defined by the user and converts it to a
-	stuffed array for use in a verilog program
-	The vast majority of this code is adapted from:
+	This script loads in an image supplied by the user and converts it to a
+	stuffed array for use in a verilog module
+	The code is heavily modified from:
 	https://github.com/Jesse-Millwood/image-2-coe
 	which, itself was adapted from a MATLAB script, "IMG2coe8.m",
 	that was found in an on-line example at: 
@@ -18,9 +23,20 @@ Description:
 	I also got some info (which probably should have been obvious)
 	from: http://computingvoyage.com/982/color-bit-depth-reducer/
 	
+	A good PIL guide is at: http://effbot.org/imagingbook/pil-index.htm
+	
 TO USE:
 	The easiest way to use this script is to copy this module to the directory
-	that contains the image that you want to convert. Then run!!!
+	that contains the image that you want to convert. Then type picfix.py!!!
+	
+	The code is easily changed but it is currently set up to work with an original
+	image the points north.
+	
+	User should ensure that the rotated picture will fit in the specified 
+	dimensions
+	
+	
+	
 '''
 # Imported Standard Modules
 import sys
@@ -28,21 +44,27 @@ from PIL import Image
 
 def Convert (ImageName):
 	
-	# Open image, get a  45 deg version, shrink to 16x16
-	img   = Image.open(ImageName)
+	# Open image, make a  45 deg version, shrink to user-specified size
+	# Save these guys out to disk, this gives a preview before you add
+	# the array to your verilog project
+	img = Image.open(ImageName)
 	img45 = img.rotate(315)
-	img45 = img.resize((16,16),Image.ANTIALIAS)
-	img   = img.resize((16,16),Image.ANTIALIAS)
-
-	# Verify that the image is in the 'RGB' mode, every pixel is described by 
-	# three bytes
+	img45 = img45.resize((outWidth,outHeight),Image.ANTIALIAS)
+	img = img.resize((outWidth,outHeight),Image.ANTIALIAS)
+	img45.save('rotated_'+ImageName, 'png')
+	img.save('shrunk_'+ImageName, 'png')
+	
+	# if necessary, convert to RGB
 	if img.mode != 'RGB':
 		img = img.convert('RGB')
 	if img45.mode != 'RGB':
 		img45 = img.convert('RGB')
 		
-	# Store Width and height of image
-	# Assuming bot images are same size
+	
+	
+	# Store Width and height of transformed image
+	# If these dont equal the specified size, something went wrong 
+	# with the above steps
 	width 	= img.size[0]
 	height	= img.size[1]
 
@@ -54,13 +76,11 @@ def Convert (ImageName):
 	oFile.write('*****  Template file for the image array\n')
 	oFile.write('*****  Produced from {}\n'.format(ImageName))
 	oFile.write('***********************************************/\n\n')
-	#oFile.write('reg	[11:0]	imageArray[{}:0];\n'.format(width * height * 2 - 1))
+	oFile.write('reg [11:0] iconPixArray[{}:0];\n'.format(width * height * 2 - 1))
 	
-	# Grab the individual pixels and scale them down
-	# 
-
+	# Grab the individual pixels and convert to 12-bit color depth
 	cnt = 0
-	for i in range (['img','img45'])
+	for i in ['img','img45']:
 		for r in range(0, height):
 			for c in range(0, width):
 				# Check for IndexError, usually occurs if the script is trying to 
@@ -90,35 +110,11 @@ def Convert (ImageName):
 				cnt += 1
 	
 	
-	# for r in range(0, height):
-		# for c in range(0, width):
-			# # Check for IndexError, usually occurs if the script is trying to 
-			# # access an element that does not exist
-			# try:
-				# R,G,B = img45.getpixel((c,r))
-			# except IndexError:
-				# print 'Index Error Occurred At:'
-				# print 'c: {}, r:{}'.format(c,r)
-				# sys.exit()
-			# # convert the value 
-			# R = hex(R*15/255)[2:]
-			# G = hex(G*15/255)[2:]
-			# B = hex(B*15/255)[2:]
-			# ColorValue = R+G+B
-			
-			# # Check for Value Error, happened when the case of the pixel being 
-			# # zero was not handled properly	
-			# try:
-				# oFile.write("iconPixArray[%d] = 12'h%3.2X;\n"%(cnt, int(ColorValue,16)))
-			# except ValueError:
-				# print 'Value Error Occurred At:'
-				# print 'Contents of ColorValue: {0} at r:{1} c:{2}'.format(ColorValue,r,c)
-				# print 'R:{0} G:{1} B{2}'.format(R,G,B)
-				# print 'Rb:{0} Gb:{1} Bb:{2}'.format(Rb,Gb,Bb)
-				# sys.exit()
-			# cnt += 1
 	
 	
+	oFile.write('\n/**********************************************\n')
+	oFile.write('*****  End of array template\n')
+	oFile.write('***********************************************/\n\n')
 	oFile.close()
 	print '\n\n\t***********************************************************\n'
 	print '\t\tDONE!!! Saved as: {}\n'.format(filename)
@@ -130,5 +126,8 @@ def Convert (ImageName):
 
 
 if __name__ == '__main__':
+	print '\n\n\n\n\n\n'
 	ImageName = raw_input('Enter the name of your image: ')
+	outHeight = int(input('Enter desired finished height: '))
+	outWidth = int(input('Enter desired finished width: '))
 	Convert(ImageName)
